882e0718d8732f6cf9b41c902dfc929ab7e6af8d
Vikash Patel <77204893+vikash-patel0@users.noreply.github.com>
2025-07-19 23:36:04 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (14.60%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,59 +10,25 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        // if(list1==null) return list2;
-        // if(list2==null) return list1;
-
-        // ListNode prev=null;
-        // ListNode next=list1;
-        // ListNode temp=list2;
-        // while(temp!=null){
-        //     if(next.val>=temp.val){
-        //         prev.next=temp;
-        //         temp.next=next;
-        //     }else{
-        //         ListNode hola=next.next;
-        //         next.next=temp;
-        //         temp.next=hola;
-        //     }
-        //     prev=next;
-        //     next=next.next;
-        //     list2=list2.next;
-        //     temp=list2;
-        // }
-        // return list1;
-
-
-        Queue<ListNode> qu= new LinkedList<>();
-
-        while (list1 != null && list2 != null) {
-            if (list1.val <= list2.val) {
-                qu.offer(list1);
-                list1 = list1.next;
-            } else {
-                qu.offer(list2);
-                list2 = list2.next;
+        if(list1==null) return list2;
+        if(list2==null) return list1;
+        ListNode head= new ListNode(-1);
+        ListNode node= head;
+        while(list1!=null && list2!=null){
+            if(list1.val<=list2.val){
+                node.next=list1;
+                list1=list1.next;
+            }else{
+                node.next=list2;
+                list2=list2.next;
             }
+            node=node.next;
         }
-        while (list1 != null) {
-            qu.offer(list1);
-            list1 = list1.next;
+        if(list1!=null){
+            node.next=list1;
+        }else{
+            node.next=list2;
         }
-        while (list2 != null) {
-            qu.offer(list2);
-            list2 = list2.next;
-        }
-        qu.offer(null);
-
-
-        ListNode head = new ListNode(-1);
-        head=qu.poll();
-        ListNode tail = head;
-
-        while (!qu.isEmpty()) {
-            tail.next = qu.poll();;
-            tail = tail.next;
-        }
-        return head;
+        return head.next;
     }
 }
