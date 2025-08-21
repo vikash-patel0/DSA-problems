d3e99862717163c5d932f2cf4f793c17daabdeaf
Vikash Patel <77204893+vikash-patel0@users.noreply.github.com>
2025-08-22 01:19:49 +0530

    Time: 1 ms (99.87%) | Memory: 52.9 MB (69.20%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,6 +1,25 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        int count=0;
+        int maj=nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(count==0){
+                maj=nums[i];
+            }
+            if(maj==nums[i]){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return maj;
     }
 }
+
+//The Boyer-Moore Voting Algorithm
+//majority elements cancels out others
+//logic is that maj element is always greater in number than rest elements all together
+// so we are basically cancelling that many times a maj element.. if different number appears
+// at the end one or more ocurrence of maj element will be left
+// we take count as 0 in starting and take starting number as our maj element
+// then we will be increasing count as we see duplicate of maj element and decrement count if we see different element... as count becomes zero that means we will assign next element as maj element and start doing operation again 
