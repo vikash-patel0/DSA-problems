0dd8df821ef0b26c3cae18a9cd78ccd390c10120
Vikash Patel <77204893+vikash-patel0@users.noreply.github.com>
2025-08-23 00:13:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (32.21%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,17 +1,19 @@
 class Solution {
     public double myPow(double x, int n) {
-        if (n == Integer.MIN_VALUE) {
-            x = x * x;
-            n = n / 2; // Now n is a safely negatable number.
+        
+        if(n<0){
+            return 1/divPow(x,n);
         }
-        if(n<0) {
-            x=1/x;
-            n=-n;
-        }//handles negative numbers
-        if(n==0) return 1.0; // stopping condition
-
-        double half= myPow(x,(int)(n/2));  //basically we are dividing the power and then squaring
-        if(n%2==0) return half*half; //for even power
-        else return half*half*x; //for odd power .. one extra x will be there
+        return divPow(x,n);
+    }
+    static double divPow(double x, long n){
+        if(x==0) return 0;
+        if(n==0) return 1;
+        double half=divPow(x,n/2);
+        if(n%2==0){
+            return half*half;
+        }else{
+            return half*half*x;
+        }
     }
 }
