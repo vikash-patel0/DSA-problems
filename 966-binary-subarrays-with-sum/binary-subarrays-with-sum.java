bb2a0104255bb2e6e4cfb73774efe4ff38261ea3
Vikash Patel <77204893+vikash-patel0@users.noreply.github.com>
2025-08-28 00:33:46 +0530

    Time: 26 ms (15.61%) | Memory: 49.9 MB (19.80%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,49 +1,20 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int k) {
-        int i=0,j=0;
-        int sum=0;
+        Map<Integer,Integer> prefixSumFreq= new HashMap<>();
         int count=0;
-        while(j!=nums.length && i!=nums.length-1){
-            if(nums[j]==k) count++;
-            sum+=nums[j];
-            int sumT=sum;
-            while(i!=j){
-                if(sumT==k) count++;
-                sumT-=nums[i];
-                i++;
+        int currSum=0;
+        for(int i=0;i<nums.length;i++){
+            currSum+=nums[i];
+            if(currSum==k) count++;
+            if(prefixSumFreq.containsKey(currSum-k)){
+                count+=prefixSumFreq.get(currSum-k);
             }
-            if(i==j){
-                j++;
-                i=0;
+            if(prefixSumFreq.containsKey(currSum)){
+                prefixSumFreq.put(currSum,prefixSumFreq.get(currSum)+1);
             }else{
-                i++;
+                prefixSumFreq.put(currSum,1);
             }
         }
-
         return count;
     }
 }
-
-
-
-// int i=0,j=0;
-//         int sum=0;
-//         int count=0;
-//         while(j<nums.length){
-//             sum+=nums[j];
-//             while(sum>goal && i<=j){
-//                 sum -= nums[i];
-//                 i++;
-//             }
-
-//             if(sum == goal) count++;
-//             int ii = i;
-//             while(ii<=j && nums[ii] == 0){
-//                 ii++;
-//                 // i++;
-//                 count++;
-//             }
-//             j++;
-//         }
-
-//         return count;
